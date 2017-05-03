edd51c10139bc87c468242ad7232a381eb44413b
fatfatfox <17758909@qq.com>
2017-05-03 14:00:52 +0800

    修改公共信息

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'AntD Admin',
+  name: '中国光大银行',
   prefix: 'antdAdmin',
-  footerText: 'Ant Design Admin 版权所有 © 2016 由 zuiidea 支持',
+  footerText: '光大银行科技部 © 2017',
   logo: 'https://t.alipayobjects.com/images/T1QUBfXo4fXXXXXXXX.png',
   iconFontUrl: '//at.alicdn.com/t/font_c4y7asse3q1cq5mi.js',
   baseURL: 'http://localhost:8000/api/v1',
